6053d6691f8a1edc71f059868d0f2b7accb33432
Ian Duncan <ian@mercury.com>
2021-08-17 14:19:08 -0500

    now with tests

--- test/DoubleBufferSpec.hs
+++ test/DoubleBufferSpec.hs
@@ -1,7 +1,10 @@
 module DoubleBufferSpec where
 
-import DataSketches.Quantiles.RelativeErrorQuantile
+import Control.Monad
+import DataSketches.Quantiles.RelativeErrorQuantile.DoubleBuffer
 import qualified Data.Vector.Unboxed as UV
+import Test.Hspec
+import qualified Data.List
 
 spec :: Spec
 spec = do
@@ -15,25 +18,78 @@ spec = do
           UV.freeze v
     it "works upwards" $ do
       v <- mergeSortTestImpl False
-      UV.fromList (sort ([0..31] ++ [0..31])) @=? v
+      v `shouldBe` UV.fromList (Data.List.sort ([0..31] ++ [0..31]))
     it "works downwards" $ do
       v <- mergeSortTestImpl True
-      UV.fromList (sort ([0..31] ++ [0..31])) @=? v
-  {-
+      v `shouldBe` UV.fromList (Data.List.sort ([0..31] ++ [0..31]))
   describe "getEvensOrOdds" $ do
     checkGetEvensOrOdds False False
     checkGetEvensOrOdds False True
     checkGetEvensOrOdds True False
     checkGetEvensOrOdds True True
-  -}
+  describe "checkAppendAndSpaceTop" $ do
+    checkAppendAndSpace True
+    checkAppendAndSpace False
+  describe "checkEnsureCapacity" $ do
+    mapM_ checkEnsureCapacity [True, False]
 
-{-
-checkGetEvensOrOdds :: Bool -> Bool -> _
+checkGetEvensOrOdds :: Bool -> Bool -> Spec
 checkGetEvensOrOdds odds spaceAtBottom = 
   it ("works for odds=" ++ show odds ++ " spaceAtBottom=" ++ show spaceAtBottom) $ do
     buf <- mkBuffer cap 0 spaceAtBottom
     forM_ [0..cap `div` 2] (append buf . fromIntegral)
-    getEvensOrOdds buf 0 (cap `div` 2) odds
+    out <- getEvensOrOdds buf 0 (cap `div` 2) odds
+    v <- UV.freeze =<< getVector out
+    v `shouldSatisfy` UV.all (\x -> if odds then floor x `mod` 2 == 1 else floor x `mod` 2 == 0)
   where
     cap = 16
--}
+
+checkAppendAndSpace :: Bool -> Spec
+checkAppendAndSpace spaceAtBottom = it ("works for spaceAtBottom=" ++ show spaceAtBottom) $ do
+  buf <- mkBuffer 2 2 spaceAtBottom
+  do
+    count_ <- getCount buf
+    count_ `shouldBe` 0
+    capacity_ <- getCapacity buf 
+    capacity_ `shouldBe` 2
+    space_ <- getSpace buf
+    space_ `shouldBe` 2
+  append buf 1
+  do
+    count_ <- getCount buf
+    count_ `shouldBe` 1
+    capacity_ <- getCapacity buf 
+    capacity_ `shouldBe` 2
+    space_ <- getSpace buf
+    space_ `shouldBe` 1
+  append buf 2
+  do
+    count_ <- getCount buf
+    count_ `shouldBe` 2
+    capacity_ <- getCapacity buf 
+    capacity_ `shouldBe` 2
+    space_ <- getSpace buf
+    space_ `shouldBe` 0
+  append buf 3
+  do
+    count_ <- getCount buf
+    count_ `shouldBe` 3
+    capacity_ <- getCapacity buf 
+    capacity_ `shouldBe` 5
+    space_ <- getSpace buf
+    space_ `shouldBe` 2
+
+checkEnsureCapacity :: Bool -> Spec
+checkEnsureCapacity spaceAtBottom = it ("works for spaceAtBottom=" ++ show spaceAtBottom) $ do
+  buf <- mkBuffer 4 2 spaceAtBottom
+  append buf 2
+  append buf 1
+  append buf 3
+  ensureCapacity buf 8
+  sort buf
+  x1 <- buf ! 0
+  x1 `shouldBe` 1
+  x2 <- buf ! 1
+  x2 `shouldBe` 2
+  x3 <- buf ! 2
+  x3 `shouldBe` 3
