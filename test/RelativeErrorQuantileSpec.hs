9f2e1c011a26c74dbc42aef752758bd2f4a06bb1
Ian Duncan <ian@mercury.com>
2021-08-18 12:02:00 -0500

    Add more failing tests

--- test/RelativeErrorQuantileSpec.hs
+++ test/RelativeErrorQuantileSpec.hs
@@ -16,6 +16,8 @@ spec = do
   bigTest Proxy 1   200 HighRanksAreAccurate (:<)  False
   bigTest Proxy 1   200 LowRanksAreAccurate  (:<)  True
 
+  mergeSpec
+
 bigTest :: Proxy 6 -> Int -> Int -> RankAccuracy -> Criterion -> Bool -> Spec
 bigTest k min_ max_ hra crit up = do
   let testName = intercalate " "
@@ -37,10 +39,27 @@ bigTest k min_ max_ hra crit up = do
         -- checkMerge sk
   it testName testContents
 
+asIO :: IO a -> IO a
+asIO = id
+
+mergeSpec :: Spec
+mergeSpec = specify "merge works" $ asIO $ do
+  s1 <- mkReqSketch HighRanksAreAccurate :: IO (ReqSketch 12 (PrimState IO))
+  mapM_ (update s1) [0..40]
+  s2 <- mkReqSketch HighRanksAreAccurate :: IO (ReqSketch 12 (PrimState IO))
+  mapM_ (update s2) [0..40]
+  s3 <- mkReqSketch HighRanksAreAccurate :: IO (ReqSketch 12 (PrimState IO))
+  mapM_ (update s3) [0..40]
+  s <- mkReqSketch HighRanksAreAccurate :: IO (ReqSketch 12 (PrimState IO))
+  s `merge` s1
+  s `merge` s2
+  s `merge` s3
+  pure ()
+
 loadSketch :: forall n. (KnownNat n, ValidK n) => Proxy n -> Int -> Int -> RankAccuracy -> Criterion -> Bool -> IO (ReqSketch n (PrimState IO))
 loadSketch k min_ max_ hra ltEq up = do
   sk <- mkReqSketch hra :: IO (ReqSketch n (PrimState IO))
-  -- TODO figure this bit out
+  -- This just seems geared at making sure that ranks come out right regardless of order
   mapM_ (update sk . fromIntegral) $ if up
     then [min_ .. max_]
     else reverse [min_ .. max_ {- + 1 -}]
