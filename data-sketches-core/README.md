34e63bf1fc7f4a42bbfa6de0a481e4771e744ddd
Ian Duncan <ian@mercury.com>
2021-08-26 16:04:39 -0400

    xunit output?

--- data-sketches-core/README.md
+++ data-sketches-core/README.md
@@ -1,1 +1,3 @@
 # data-sketches-core
+
+https://datasketches.apache.org/api/java/snapshot/apidocs/overview-summary.html
--- data-sketches/test/Spec.hs
+++ data-sketches/test/Spec.hs
@@ -4,12 +4,25 @@ import qualified CompactorSpec
 import qualified DoubleBufferSpec
 import qualified ProofCheckSpec
 import qualified RelativeErrorQuantileSpec
+import System.Environment
+import Test.HSpec.JUnit
+import Test.Hspec.Runner
 
 main :: IO ()
-main = hspec $ do
-  describe "Auxiliary" AuxiliarySpec.spec
-  describe "Compactor" CompactorSpec.spec
-  describe "DoubleBuffer" DoubleBufferSpec.spec
-  describe "ProofCheck" ProofCheckSpec.spec
-  describe "RelativeErrorQuantile" RelativeErrorQuantileSpec.spec
+main = do
+  isCI <- lookupEnv "CI"
+  args <- getArgs
+  >>= readConfig config
+  >>= withArgs [] . runSpec specs
+  >>= evaluateSummary
+  where
+    config = defaultConfig 
+      { configFormat = junitFormat "test-results.xml" "data-sketches" 
+      }
+    specs = do
+      describe "Auxiliary" AuxiliarySpec.spec
+      describe "Compactor" CompactorSpec.spec
+      describe "DoubleBuffer" DoubleBufferSpec.spec
+      describe "ProofCheck" ProofCheckSpec.spec
+      describe "RelativeErrorQuantile" RelativeErrorQuantileSpec.spec
 
